34564de3e2d8171460f4e98afb6e43b61659bfc1
yuxristin <n453ru@gmail.com>
2023-04-22 13:27:49 +0300

    3 ДЗ

--- home_work/3_hw.py
+++ home_work/3_hw.py
@@ -37,3 +37,46 @@ def rand3(random_number1 = random.randint(1, 12),
     else:
         print(random_number1, 'Осень','Завершилась третья','Началась четвертая',sep = '\n')
 rand3()
+
+def rand4(rn1 = random.randint(-9999, 9999),rn2 = random.randint(-9999, 9999),
+          rn3 = random.randint(-9999, 9999),rn4 = random.randint(-9999, 9999),
+          sep = '') ->None:
+    if rn1 > 10 and rn2 > 10 and rn3 > 10 and rn4 > 10:
+        print(rn1,rn2,rn3,rn4,'Y','Завершилась четвертая','Началась пятая',sep = '\n')
+    else:
+        print(rn1,rn2,rn3,rn4,'N','Завершилась четвертая','Началась пятая',sep = '\n')
+rand4()
+
+def rand5(rn1 = random.randint(-9999, 9999), rn2 = random.randint(-9999, 9999),
+          rn3 = random.randint(-9999, 9999),rn4 = random.randint(-9999, 9999),
+          rn5 = random.randint(-9999, 9999),
+          pol: int = 0,
+          otr: int = 0,
+          sep = '') ->None:
+    if rn1 > 0:
+        pol = pol + 1
+    else:
+        otr = otr + 1
+    if rn2 > 0:
+        pol = pol + 1
+    else:
+        otr = otr + 1
+    if rn3 > 0:
+        pol = pol + 1
+    else:
+        otr = otr + 1
+    if rn4 > 0:
+        pol = pol + 1
+    else:
+        otr = otr + 1
+    if rn5 > 0:
+        pol = pol + 1
+    else:
+        otr = otr + 1
+        print(rn1, rn2, rn3, rn4,rn5, 'Количество положительных = ', pol,'Шестая задача', sep ='\n')
+rand5()
+
+def rand6(let: int = random.randint(0, 100), mes: int = random.randint(0, 9999),
+          d = 29, sep = '') ->None:
+        print(let * 12 + mes * d)
+rand6()
