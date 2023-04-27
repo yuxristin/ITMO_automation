e0c41bbdd7960a7e6683c7644e0226eefb84920f
yuxristin <n453ru@gmail.com>
2023-04-28 01:26:25 +0300

    5hw

--- home_work/5_hw.py
+++ home_work/5_hw.py
@@ -4,21 +4,32 @@ from selenium.webdriver.common.by import By
 driver = webdriver.Chrome()
 driver.get('https://www.saucedemo.com/')
 
+
 pole1 = driver.find_element(By.CSS_SELECTOR, '#user-name')
 if pole1 is None:
     print('Not find')
 else:
     print('Got Element')
 
-pole1 = driver.find_element(By.CSS_SELECTOR, '#password')
-if pole1 is None:
+pole2 = driver.find_element(By.CSS_SELECTOR, '#password')
+if pole2 is None:
     print('Not find')
 else:
     print('Got Element')
 
 button1 = driver.find_element(By.CSS_SELECTOR, '#login-button')
-if pole1 is None:
+if button1 is None:
     print('Not find')
 else:
     print('Got Element')
+
+list = [pole1, pole1, button1]
+
+def get_number_of_elements(list):
+    count = 0
+    for element in list:
+        count += 1
+    return count
+
+print("Number of elements in the list: ", get_number_of_elements(list))
 
